930b73add99d2314639faec981518cba26c00d2a
z3y <z3y@protonmail.com>
2025-12-09 13:21:49 +0100

    day 8 part 2

---
+++ day8/part2/main.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <charconv>
+#include <vector>
+#include <cmath>
+#include <memory>
+#include <algorithm>
+#include <format>
+
+struct Vec3
+{
+    int64_t x;
+    int64_t y;
+    int64_t z;
+
+    Vec3(int64_t x_, int64_t y_, int64_t z_) : x(x_), y(y_), z(z_) {}
+    Vec3() : x(0), y(0), z(0) {}
+
+    Vec3 operator-(const Vec3 &other) const
+    {
+        return Vec3(x - other.x, y - other.y, z - other.z);
+    }
+
+    Vec3 operator+(const Vec3 &other) const
+    {
+        return Vec3(x + other.x, y + other.y, z + other.z);
+    }
+
+    bool operator==(const Vec3 &other) const
+    {
+        return x == other.x && y == other.y && z == other.z;
+    }
+
+    static int64_t Dot(const Vec3 &a, const Vec3 &b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    static int64_t DistanceInt(const Vec3 &a, const Vec3 &b)
+    {
+        Vec3 v = a - b;
+        return Dot(v, v);
+    }
+
+    std::string ToString()
+    {
+        return std::format("({0}, {1}, {2})", x, y, z);
+    }
+};
+
+struct Pair
+{
+    Vec3 a;
+    Vec3 b;
+};
+
+struct Circuit
+{
+    std::vector<Vec3> points;
+};
+
+int32_t MergeCircuits(std::vector<Circuit> &circuits)
+{
+    int32_t mergeCount = 0;
+    for (size_t i = 0; i < circuits.size(); i++)
+    {
+        for (size_t j = i + 1; j < circuits.size();)
+        {
+            bool shouldMerge = false;
+
+            for (const Vec3 &p : circuits[i].points)
+            {
+                if (std::find(circuits[j].points.begin(), circuits[j].points.end(), p) != circuits[j].points.end())
+                {
+                    shouldMerge = true;
+                    break;
+                }
+            }
+
+            if (shouldMerge)
+            {
+                for (const Vec3 &p : circuits[j].points)
+                {
+                    if (std::find(circuits[i].points.begin(), circuits[i].points.end(), p) == circuits[i].points.end())
+                    {
+                        circuits[i].points.push_back(p);
+                    }
+                }
+
+                // std::printf("merged %ld-%ld\n", circuits[i].points.size(), circuits[j].points.size());
+                circuits[j].points.clear();
+                mergeCount++;
+            }
+            else
+            {
+                ++j;
+            }
+        }
+    }
+
+    return mergeCount;
+}
+
+int main()
+{
+    std::ifstream file("input.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file\n";
+        return 1;
+    }
+
+    std::string line;
+
+    std::vector<Vec3> points;
+
+    while (std::getline(file, line))
+    {
+        Vec3 p;
+
+        size_t split0 = line.find(",");
+
+        p.x = std::stoi(line.substr(0, split0));
+        std::string part2 = line.substr(split0 + 1);
+
+        size_t split1 = part2.find(",");
+
+        p.y = std::stoi(part2.substr(0, split1));
+        p.z = std::stoi(part2.substr(split1 + 1));
+
+        points.push_back(p);
+    }
+
+    std::vector<Pair> pairs;
+
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        for (size_t j = i + 1; j < points.size(); j++)
+        {
+            Pair pair;
+            pair.a = points[i];
+            pair.b = points[j];
+
+            pairs.push_back(pair);
+        }
+    }
+
+    std::sort(pairs.begin(), pairs.end(),
+              [](const Pair &lhs, const Pair &rhs)
+              {
+                  return Vec3::DistanceInt(lhs.a, lhs.b) < Vec3::DistanceInt(rhs.a, rhs.b);
+              });
+
+    std::vector<Circuit> circuits;
+
+    Pair lastBox;
+
+    int32_t circuitsCount = points.size();
+
+    for (size_t i = 0; i < pairs.size(); i++)
+    {
+        if (circuitsCount == 1)
+        {
+            break;
+        }
+
+        Pair p = pairs[i];
+
+        bool circuitFound = false;
+        for (size_t j = 0; j < circuits.size(); j++)
+        {
+            auto &points = circuits[j].points;
+
+            bool aExists = std::find(points.begin(), points.end(), p.a) != points.end();
+            bool bExists = std::find(points.begin(), points.end(), p.b) != points.end();
+
+            if (aExists || bExists)
+            {
+                if (!aExists)
+                {
+                    circuits[j].points.push_back(p.a);
+                    circuitsCount--;
+                }
+                if (!bExists)
+                {
+                    circuits[j].points.push_back(p.b);
+                    circuitsCount--;
+                }
+
+                lastBox = p;
+
+                circuitFound = true;
+                int32_t mergeCount = MergeCircuits(circuits);
+
+                break;
+            }
+        }
+
+        if (!circuitFound)
+        {
+            Circuit newCircuit;
+            newCircuit.points.push_back(p.a);
+            newCircuit.points.push_back(p.b);
+            lastBox = p;
+            circuitsCount--;
+
+            circuits.push_back(newCircuit);
+        }
+    }
+
+    std::printf("a: %s, b: %s\n", lastBox.a.ToString().c_str(), lastBox.b.ToString().c_str());
+    std::printf("x: %ld\n", lastBox.a.x * lastBox.b.x);
+
+    return 0;
+}
